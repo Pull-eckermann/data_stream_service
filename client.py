df2e9cb3aa8a43800b14b260ee837d78c2e9451c
Erick.Cardoso <Erick.Cardoso@ipinfusion.com>
2023-11-08 17:33:28 -0300

    Finished

--- client.py
+++ client.py
@@ -4,19 +4,21 @@ import threading
 from getkey import getkey
 import utils
 
+# A thread will check if user press "q" or "space-bar" keys
 def check_input():
     global close_client_flag, operation
     while True:
         key = getkey()
-        if key == 'q':
+        if key == 'q': # Close clients connection and ends execution
             close_client_flag = True
             break
-        elif key == ' ':
+        elif key == ' ': # switch to show music name mode or show music lyrics
             os.system('clear')
             operation = True if operation == False else False
 
+# Check is the command line is correct
 if len(sys.argv) != 2:
-    print('Uso correto: python3 client.py <Número da porta>')
+    print('Correct usage: python3 client.py <Port number>')
     exit(1)
 
 # Clear console terminal
@@ -36,25 +38,33 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP Socket
 UDP_PORT_RECV = int(sys.argv[1])
 try:
     sock.bind((UDP_IP, UDP_PORT_RECV))
-except:
-    print('Erro: Porta expecificada já em uso, por favor informe outra.')
+except: # Check if UDP port is already in use
+    print('Error: Porta expecificada já em uso, por favor informe outra.')
     exit(1)
 
+# Send connection request
 print("Sending Hello message...")
 sock.sendto(b'Hello', dest)
+
+# Start a thread to check user's input in a non-bloquing way 
 thread = threading.Thread(target=check_input, args=())
 thread.start()
 
 while True:
+    # Wait for the package from the server
     data = utils.receiv(sock)
     seq_number = data[0]
     msg = data[1]
     
+    # Accounts the number of packages that arrived out of order
     if seq_number > last_seq_number:
         if seq_number != last_seq_number + 1:
-            lost_pkg_counter += 1
+            lost_pkg_counter += (seq_number - last_seq_number) - 1 # Accounts the number of lost packages.
+        
         music_name = msg.split('|')[0]
         line = msg.split('|')[1]
+
+        # Operation specifies if just the music name is shown of if the lyric line is shown
         if operation:
             os.system('clear')
             print('Now playing '+ music_name)
@@ -72,6 +82,7 @@ while True:
     else:
         outOrderPkg += 1
 
+    # Close the connection with the server
     if close_client_flag:
         message = b'bye'
         print('Sending bye message...')
@@ -79,6 +90,7 @@ while True:
         sock.close()
         break   
 
+# Print report
 print("Closing connection to the server...")
 print('Total Out of Order Packets received: {}'.format(outOrderPkg))
 print('Total lost packages count: {}'.format(lost_pkg_counter))
--- server.py
+++ server.py
@@ -14,9 +14,9 @@ def msgGenerator():
                 lyric_line = '*Now playing {} ...\n'.format(music_name)
             else:
                 lyric_line = line
-            sleep(sleep_interval)
+            sleep(sleep_interval) # Interval of gerneration of the messages
 
-# Get the message from a global variable
+# Get the message that will be sent to client from a global variable
 def getMessage():
     global music_name, lyric_line
     msg = music_name + '|' + lyric_line
@@ -33,43 +33,40 @@ def comunica_cliente(client_addr, data):
         msg = getMessage()
         seq = seq + 1
         utils.send(sock, client_addr, (seq, msg))
-        #sock.sendto(msg.encode(), client_addr)
         sleep(sleep_interval)
+
     print("{} Closed connection to the server...".format(client_addr))
     print("Finishing thread...")
     print("TOTAL CLIENTS ",threading.active_count()-2)
 
 # ===================== Start code =====================
-RUNF = {}
+RUNF = {} # This dictionary makes the control of witch client thread is active
 lyric_line = ''
 music_name = ''
 musics_file = open("musicas.txt", "r")
 musics = musics_file.readlines()
 musics_file.close()
 
+# If the sleep interval is defined, it will be used. Defaul is 2 seconds...
 if len(sys.argv) > 1:
     sleep_interval = float(sys.argv[1])
 else:
     sleep_interval = 2
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 6454
+UDP_IP = "127.0.0.1" # Loopback
+UDP_PORT = 6454 # Server UDP port
 server_addr = (UDP_IP, UDP_PORT)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP Socket
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(server_addr)
 
-RUNF[str(server_addr)] = False
-
+# Start a new thread that will start to generate the messages
 thread = threading.Thread(target=msgGenerator, args=())
 thread.start()
 
 while True:
-    if RUNF[str(server_addr)]:
-        break
-
-    try:
+    try: # Principal thread wait to receive connection request from clients
         data, client_addr = sock.recvfrom(20)
     except:
         data = False
@@ -82,7 +79,7 @@ while True:
     elif data == b'Hello':
         print('Got connection request from {} with message {}. Creating a new thread...'.format(client_addr, data))
         thread = threading.Thread(target=comunica_cliente, args=(client_addr, data))
-        thread.start()
+        thread.start() # Starts a new thread that will comunicat to each client
         print("TOTAL CLIENTS ",threading.active_count()-2)
 
     sleep(0.1)
